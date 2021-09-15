8a7f85a02ee2ce1eda65e501fa7840876e228a38
agomes94 <arthur.f.eaa@gmail.com>
2021-09-15 12:02:15 -0300

    update

--- resultados/gerar_resultado.py
+++ resultados/gerar_resultado.py
@@ -1,6 +1,6 @@
 words = open("GCP/resultado_part-00000.txt", "r")
 resultado = open("GCP/resultado.txt", "w")
 i = 0
-while i <= 10:
+while i < 10:
     resultado.writelines(words.readline())
     i += 1
